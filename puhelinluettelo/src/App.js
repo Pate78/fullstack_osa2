fd5ed07aecffcb8102cdc9cb75096a9070f25fdb
Pate78 <patrick.pitkanen@gmail.com>
2019-08-24 15:20:09 +0300

    valmis

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -1,16 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Contacts from './components/Contacts'
 import Filter from './components/Filter'
 import AddContact from './components/AddContact'
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '0400-333 4444' },
-    { name: 'Teppo Tuunaaja', number: '040-7654321'}
-  ]) 
+  const [ persons, setPersons] = useState([]) 
   const [ newName, setNewName ] = useState('Uusi nimi')
   const [ newNumber, setNewNumber ] = useState('040-1234567')
   const [newFilter, setNewFilter] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+
   const filteredPersons = newFilter !== undefined ? 
     persons.filter((person) => person.name.includes(newFilter) ) : persons
 
